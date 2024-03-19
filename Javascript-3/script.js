99103bef3097b52e5bd87b7c04986419e9b727da
matterisnanto <rachmat.trisnanto186@gmail.com>
2024-03-19 12:27:33 +0700

    update

--- Javascript-3/script.js
+++ Javascript-3/script.js
@@ -1,8 +1,20 @@
 function input(){
     let nama = document.getElementById('frm').nama.value;
-    let pekerjaan = document.getElementById('frm').pekerjaan.value;
-    let hobi = document.getElementById('frm').hobi.value;
-    let input = 'Hasil Input Anda' + '<br>nama : ' + nama + '<br>pekerjaan : ' + pekerjaan + '<br>hobi : ' + hobi;
+    let pekerjaan = frm.pekerjaan.value;
+    let hobi = frm.hobi.value;
+    let input = `
+    <tr>
+        <td>nama</td>
+        <td>${nama}</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+    </tr>`
     let no_input = 'maaf data anda belum terisi';
     let hasil = (nama && pekerjaan && hobi != '') ? input : no_input;
 
