e7a2757e25e0d280153d2dfdf46e66103813f429
matterisnanto <rachmat.trisnanto186@gmail.com>
2024-03-28 23:39:38 +0700

    upload tugas 3 pemograman php

---
+++ PHP-3/index.php
@@ -0,0 +1,248 @@
+<?php
+$m1 = ['nama' => 'Amran Frenly Panjaitan', 'nim' => '21210037', 'nilai' => 20];
+$m2 = ['nama' => 'Vino Zaky Edria', 'nim' => '065120181', 'nilai' => 63];
+$m3 = ['nama' => 'Arrauuf Reza Firmansyah', 'nim' => '20210801188', 'nilai' => 63];
+$m4 = ['nama' => 'Amanda Sari', 'nim' => '21220529', 'nilai' => 82];
+$m5 = ['nama' => 'Moh Farrij Wajdi', 'nim' => 'E41210223', 'nilai' => 91];
+$m6 = ['nama' => 'Dewa Gede Andika Andara Putra', 'nim' => '2113101017', 'nilai' => 69];
+$m7 = ['nama' => 'Abdul Fatah', 'nim' => '202143501891', 'nilai' => 56];
+$m8 = ['nama' => 'Muhammad Akbar Mustajabah', 'nim' => '2143015', 'nilai' => 73];
+$m9 = ['nama' => 'Zakiyyah Nurfida', 'nim' => '0110121191', 'nilai' => 68];
+$m10 = ['nama' => 'Sabardi', 'nim' => '5920121038', 'nilai' => 87];
+$m11 = ['nama' => 'Genta Swarawisesa Erliarto Putra', 'nim' => '10121503', 'nilai' => 98];
+$m12 = ['nama' => 'Achmad Zakiyudin', 'nim' => '202169040028', 'nilai' => 54];
+$m13 = ['nama' => 'Shofiyyah Nur Kamilah', 'nim' => '0110121182', 'nilai' => 92];
+$m14 = ['nama' => 'Fauzan Hidayat', 'nim' => '1101211094', 'nilai' => 25];
+$m15 = ['nama' => 'Ahmad Dzaky Santino', 'nim' => '10121071', 'nilai' => 60];
+$m16 = ['nama' => 'Wisnu Munandar', 'nim' => '21SA1226', 'nilai' => 38];
+$m17 = ['nama' => 'Daffa Cesario Safi\'I', 'nim' => '1152000013', 'nilai' => 68];
+$m18 = ['nama' => 'Salman Alvaricy', 'nim' => '21105111078', 'nilai' => 91];
+$m19 = ['nama' => 'Gigih Zhafrans Mukti Luhur', 'nim' => '0110221087', 'nilai' => 62];
+$m20 = ['nama' => 'Muhammad Ikram Jundulloh', 'nim' => '0110121004', 'nilai' => 21];
+$m21 = ['nama' => 'Muhammad Hisyam Mubarak', 'nim' => '0110220068', 'nilai' => 73];
+$m22 = ['nama' => 'Muhammad Salsabil', 'nim' => '0110221178', 'nilai' => 80];
+$m23 = ['nama' => 'Salman Alfariz', 'nim' => '1101211026', 'nilai' => 65];
+$m24 = ['nama' => 'Alifia Dini Mediani Putri', 'nim' => '1102211004', 'nilai' => 50];
+$m25 = ['nama' => 'M. Asroful Anam', 'nim' => '1101211018', 'nilai' => 74];
+$m26 = ['nama' => 'Kurniasih', 'nim' => '2222475589', 'nilai' => 98];
+$m27 = ['nama' => 'Alif Wijaya', 'nim' => '0110121018', 'nilai' => 91];
+$m28 = ['nama' => 'Muhammad Azrul Mustafa', 'nim' => '2214004', 'nilai' => 65];
+$m29 = ['nama' => 'Anggi Alfian', 'nim' => '0110221042', 'nilai' => 25];
+$m30 = ['nama' => 'Muhamad Nasir', 'nim' => '1101211082', 'nilai' => 71];
+$m31 = ['nama' => 'Niki Juniar Barasi Laia', 'nim' => '210840045', 'nilai' => 91];
+$m32 = ['nama' => 'Iqlal Wira Prasetio', 'nim' => '20210410700013', 'nilai' => 77];
+$m33 = ['nama' => 'Nur Muhamad Nabil', 'nim' => '0110220291', 'nilai' => 29];
+$m34 = ['nama' => 'Muh. Farhan', 'nim' => '210205501003', 'nilai' => 74];
+$m35 = ['nama' => 'Siti Hartati', 'nim' => '210402104', 'nilai' => 82];
+$m36 = ['nama' => 'Putri Handayani', 'nim' => '210402112', 'nilai' => 22];
+$m37 = ['nama' => 'M. Irfan Hamdi', 'nim' => '211100065', 'nilai' => 56];
+$m38 = ['nama' => 'Gilang Akmal Maulana', 'nim' => '10121513', 'nilai' => 98];
+$m39 = ['nama' => 'Jimmy Wira Arba\'A', 'nim' => '21101152630296', 'nilai' => 36];
+$m40 = ['nama' => 'Muhammad Faris', 'nim' => '0110221247', 'nilai' => 97];
+$m41 = ['nama' => 'Diar Putri Yani', 'nim' => '20210120062', 'nilai' => 44];
+$m42 = ['nama' => 'Fina Orivia Nurfadillah', 'nim' => '2141720256', 'nilai' => 69];
+$m43 = ['nama' => 'Damayanti Nasution', 'nim' => '210402069', 'nilai' => 43];
+$m44 = ['nama' => 'Ahmad Ali Zulfakor', 'nim' => '20210801074', 'nilai' => 25];
+$m45 = ['nama' => 'Miftah Farid Ramdhani', 'nim' => '362101011', 'nilai' => 20];
+$m46 = ['nama' => 'Muhamad Arif Sandaya', 'nim' => '210402139', 'nilai' => 54];
+$m47 = ['nama' => 'Safina Faradilla Hasibuan', 'nim' => '41821010110', 'nilai' => 89];
+$m48 = ['nama' => 'Mochamad Ilham Shalsabiela', 'nim' => '202151080', 'nilai' => 76];
+$m49 = ['nama' => 'Ahmad Fahruddin', 'nim' => '202151054', 'nilai' => 22];
+$m50 = ['nama' => 'Calvin Prakoso Setyo Aji', 'nim' => '202151046', 'nilai' => 92];
+$m51 = ['nama' => 'Bayu Angga Saputra', 'nim' => '202151078', 'nilai' => 55];
+$m52 = ['nama' => 'Anugrah Dwi Saputra', 'nim' => '202151077', 'nilai' => 97];
+$m53 = ['nama' => 'Zavira Raihana Salsabila', 'nim' => '202151064', 'nilai' => 24];
+$m54 = ['nama' => 'Aditya Rafadilla', 'nim' => '0110221325', 'nilai' => 93];
+$m55 = ['nama' => 'Qunita Afif Mirror Khusnul', 'nim' => '1462100256', 'nilai' => 91];
+$m56 = ['nama' => 'Azharul Anam', 'nim' => '0110221135', 'nilai' => 22];
+$m57 = ['nama' => 'Muhammad Ilham Yahya', 'nim' => '212410101033', 'nilai' => 97];
+$m58 = ['nama' => 'Tengku Muhammad Zainul Aprilizar', 'nim' => '210402071', 'nilai' => 25];
+$m59 = ['nama' => 'Rifqi Zaidan Abiyan', 'nim' => '11221108', 'nilai' => 53];
+$m60 = ['nama' => 'Bagus Aji Hapsoro', 'nim' => '2111501173', 'nilai' => 52];
+$m61 = ['nama' => 'Arman Sholihin', 'nim' => '1402021010', 'nilai' => 47];
+$m62 = ['nama' => 'Nabilah Maulia Azzahra', 'nim' => '202151208', 'nilai' => 30];
+$m63 = ['nama' => 'Maulana Nofa Karisma', 'nim' => '202151087', 'nilai' => 42];
+$m64 = ['nama' => 'Diki Sunanda', 'nim' => '20210810096', 'nilai' => 68];
+$m65 = ['nama' => 'Humam Rafi Rachmad', 'nim' => '210801005', 'nilai' => 22];
+$m66 = ['nama' => 'Faishal Fitra Ramadhan', 'nim' => '08011282126060', 'nilai' => 26];
+$m67 = ['nama' => 'Aurel Fransisca Kusuma Wardhani', 'nim' => '2113030065', 'nilai' => 88];
+$m68 = ['nama' => 'Mohamad Adyaksa Azky Filoshofi', 'nim' => 'G211210106', 'nilai' => 92];
+$m69 = ['nama' => 'Faturrahman Ardiansyah', 'nim' => '2155201142', 'nilai' => 48];
+$m70 = ['nama' => 'M Nurul Alam', 'nim' => '21.240.0098', 'nilai' => 30];
+$m71 = ['nama' => 'Candra Setiawan', 'nim' => '20210810129', 'nilai' => 93];
+$m72 = ['nama' => 'Muhammad Hafis Gunawan Purba', 'nim' => '2021020254', 'nilai' => 43];
+$m73 = ['nama' => 'Achmad Alvin Ardiansyah', 'nim' => '20051214052', 'nilai' => 24];
+$m74 = ['nama' => 'Sulthon Muhtarom Putra Abadi', 'nim' => 'E41212243', 'nilai' => 20];
+$m75 = ['nama' => 'Ahmad Jazim Irsyaduddin', 'nim' => '21410100038', 'nilai' => 50];
+$m76 = ['nama' => 'Nadia Nur Ismalia', 'nim' => '21SA1148', 'nilai' => 82];
+$m77 = ['nama' => 'Ahmad Thufail', 'nim' => '2102310035', 'nilai' => 83];
+$m78 = ['nama' => 'Fawaz Abdul Aziz', 'nim' => '15200217', 'nilai' => 35];
+$m79 = ['nama' => 'Farwis Ilmi', 'nim' => '202210079', 'nilai' => 29];
+$m80 = ['nama' => 'Viviana Hanifa', 'nim' => '211011401584', 'nilai' => 86];
+$m81 = ['nama' => 'Annisa Luthfi Fadillah', 'nim' => '09021282126049', 'nilai' => 71];
+$m82 = ['nama' => 'Rachmat Ashari', 'nim' => '20210120030', 'nilai' => 28];
+$m83 = ['nama' => 'Rozir Wobari', 'nim' => '10121272', 'nilai' => 98];
+$m84 = ['nama' => 'Moh. Syukron Katsir', 'nim' => '2102310043', 'nilai' => 23];
+$m85 = ['nama' => 'Tirta Samara', 'nim' => '10121195', 'nilai' => 97];
+$m86 = ['nama' => 'Riri Angraini', 'nim' => '210402103', 'nilai' => 24];
+$m87 = ['nama' => 'Ilyasha Erfrian', 'nim' => '0110121011', 'nilai' => 20];
+$m88 = ['nama' => 'Yasir M Khatami', 'nim' => '211100061', 'nilai' => 80];
+$m89 = ['nama' => 'Dimas Rifqi Ramadhani', 'nim' => '062021107436', 'nilai' => 98];
+$m90 = ['nama' => 'Irgo Satria Bakti', 'nim' => '1842101687', 'nilai' => 49];
+$m91 = ['nama' => 'Alfi Muhammad Dzikra', 'nim' => '20210801237', 'nilai' => 96];
+$m92 = ['nama' => 'Andree Maldini Kurniawan', 'nim' => '20210801059', 'nilai' => 59];
+$m93 = ['nama' => 'Muhammad Fadilah', 'nim' => '21210039', 'nilai' => 67];
+$m94 = ['nama' => 'Faqihul Muqoddam', 'nim' => '362155401126', 'nilai' => 38];
+$m95 = ['nama' => 'Tirta Raja', 'nim' => '0110121002', 'nilai' => 63];
+$m96 = ['nama' => 'Muhammad Hadyan Yazid', 'nim' => '211112533', 'nilai' => 50];
+$m97 = ['nama' => 'Sarah Khoirunnisa', 'nim' => '20210810042', 'nilai' => 28];
+$m98 = ['nama' => 'Rachmat Trisnanto', 'nim' => '19200176', 'nilai' => 98];
+$m99 = ['nama' => 'Dimas Aulia Rahman', 'nim' => '50421384', 'nilai' => 45];
+$m100 = ['nama' => 'Muh. Padli', 'nim' => 'D0221513', 'nilai' => 24];
+
+$daftar_m = [
+  $m1, $m2, $m3, $m4, $m5,
+  $m6, $m7, $m8, $m9, $m10,
+  $m11, $m12, $m13, $m14, $m15,
+  $m16, $m17, $m18, $m19, $m20,
+  $m21, $m22, $m23, $m24, $m25,
+  $m26, $m27, $m28, $m29, $m30,
+  $m31, $m32, $m33, $m34, $m35,
+  $m36, $m37, $m38, $m39, $m40,
+  $m41, $m42, $m43, $m44, $m45,
+  $m46, $m47, $m48, $m49, $m50,
+  $m51, $m52, $m53, $m54, $m55,
+  $m56, $m57, $m58, $m59, $m60,
+  $m61, $m62, $m63, $m64, $m65,
+  $m66, $m67, $m68, $m69, $m70,
+  $m71, $m72, $m73, $m74, $m75,
+  $m76, $m77, $m78, $m79, $m80,
+  $m81, $m82, $m83, $m84, $m85,
+  $m86, $m87, $m88, $m89, $m90,
+  $m91, $m92, $m93, $m94, $m95,
+  $m96, $m97, $m98, $m99, $m100,
+];
+
+// deklarasikan nama2 judul pada tabel header menggunakan looping array
+$judul = ['No', 'Nama', 'NIM', 'Nilai', 'Keterangan', 'Grade', 'Predikat'];
+// fungsi agregat di array
+$jumlah_nilai = array_column($daftar_m, 'nilai');
+$total_nilai = array_sum($jumlah_nilai);
+$nilai_tertinggi = max($jumlah_nilai);
+$nilai_terendah = min($jumlah_nilai);
+$jumlah_mahasiswa = count($daftar_m);
+$nilai_rata2 = $total_nilai / $jumlah_mahasiswa;
+$keterangan = [
+'Nilai Tertinggi' => $nilai_tertinggi,
+'Nilai Terendah' => $nilai_terendah,
+'Nilai Rata-rata' => $nilai_rata2,
+'Jumlah Mahasiswa' => $jumlah_mahasiswa,
+'Jumlah Keseluruhan Nilai' => $total_nilai,
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.datatables.net/2.0.3/css/dataTables.bootstrap5.css"
+    />
+  </head>
+  <body>
+    <div class="container-md mt-4">
+      <h2 class="center">Daftar Nilai Mahasiswa</h2>
+      <!-- <table id="mahasiswa" class="table table-dark table-striped table-hover"> -->
+      <table
+        id="mahasiswa"
+        class="table table-striped table-hover"
+        style="width: 100%"
+      >
+        <thead>
+          <tr>
+            <?php
+              foreach ($judul as $judulan) { ?> 
+              <th><?php echo $judulan; ?></th>
+            <?php } ?>
+          </tr>
+        </thead>
+        <tbody>
+            <?php
+
+            $no = 1;
+foreach ($daftar_m as $mahasiswa) {
+    $kelulusan = ($mahasiswa['nilai'] >= 65) ? 'Lulus' : 'Gagal';
+    if ($mahasiswa['nilai'] >= 85 && $mahasiswa['nilai'] <= 100) {
+        $grade = 'A';
+    } elseif ($mahasiswa['nilai'] >= 75 && $mahasiswa['nilai'] <= 84) {
+        $grade = 'B';
+    } elseif ($mahasiswa['nilai'] >= 65 && $mahasiswa['nilai'] <= 74) {
+        $grade = 'C';
+    } elseif ($mahasiswa['nilai'] >= 55 && $mahasiswa['nilai'] <= 64) {
+        $grade = 'D';
+    } else {
+        $grade = 'E';
+    }
+
+    switch ($grade) {
+        case 'A':
+            $predikat = 'Memuaskan';
+            break;
+        case 'B':
+            $predikat = 'Bagus';
+            break;
+        case 'C':
+            $predikat = 'Cukup';
+            break;
+        case 'D':
+            $predikat = 'Kurang';
+            break;
+        case 'E':
+            $predikat = 'Buruk';
+            break;
+        default:
+            break;
+    }
+
+    ?>
+            <tr>
+              <td><?php echo $no++; ?></td>
+              <td><?php echo $mahasiswa['nama']; ?></td> 
+              <td><?php echo $mahasiswa['nim']; ?></td>
+              <td><?php echo $mahasiswa['nilai']; ?></td> 
+              <td><?php echo $kelulusan; ?></td> 
+              <td><?php echo $grade; ?></td> 
+              <td><?php echo $predikat; ?></td> 
+            </tr>
+            <?php } ?>
+        </tbody>
+        <tfoot>
+          <?php foreach ($keterangan as $ket => $hasil) {
+              ?>
+          <tr>
+            <th colspan="3"><?php echo $ket; ?></th>
+            <th colspan="4"><?php echo $hasil; ?> </th>
+          </tr>
+          <?php } ?>
+        </tfoot>
+      </table>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.0.3/js/dataTables.bootstrap5.js"></script>
+    <script>
+      new DataTable("#mahasiswa");
+    </script>
+  </body>
+</html>
